89f3e38018004082b734694730cb1efa17c5b1b2
Luis Gerardo Delgado <luisgdm93@gmail.com>
2014-03-03 11:00:33 -0600

    First commit

---
+++ BreakingBad/src/breakingbad/Canasta.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package breakingbad;
+
+import breakingbad.Animacion;
+
+/**
+ *
+ * @author Luis
+ */
+
+//import java.awt.Image;
+
+public class Canasta extends Objeto {
+    
+    public Canasta(int posX, int posY, Animacion animacion) {
+        
+        super(posX, posY, animacion);
+        
+    }
+    
+}
---
+++ BreakingBad/src/breakingbad/Objeto.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package breakingbad;
+
+/**
+ *
+ * @author Luis
+ */
+
+import javax.swing.ImageIcon;
+import java.awt.Image;
+import java.awt.Rectangle;
+
+
+public class Objeto {
+    
+    private int posX;
+    private int posY;
+    private Animacion animacion;  
+    
+    public Objeto(int posX, int posY, Animacion animacion) {
+        
+        this.posX = posX;
+        this.posY = posY;
+        this.animacion = animacion;
+        
+    }
+    
+    public void setPosX(int posX) {
+        
+        this.posX = posX;
+        
+    }
+    
+    public int getPosX() {
+        
+        return posX;
+        
+    }
+    
+    public void setPosY(int posY) {
+        
+        this.posY = posY;
+        
+    }
+    
+    public int getPosY() {
+        
+        return posY;
+        
+    }
+    
+    public void setImageIcon(Animacion animacion) {
+        
+        this.animacion = animacion;
+        
+    }
+    
+    public ImageIcon getImageIcon() {
+        
+        return animacion.getImagen();
+        
+    }
+    
+    public int getAncho() {
+        
+        return animacion.getImagen().getIconWidth();
+    
+    }
+    
+    public int getAlto() {
+        
+        return animacion.getImagen().getIconHeight();
+        
+    }
+    
+    public Image getImagenI() {
+        
+        return animacion.getImagen().getImage();
+        
+    }
+    
+    public Rectangle getPerimetro() {
+        
+        return new Rectangle(getPosX(), getPosY(), getAncho(), getAlto());
+        
+    }
+    
+    public boolean intersecta(Objeto obj) {
+        
+        return getPerimetro().intersects(obj.getPerimetro());
+        
+    }
+        
+}
